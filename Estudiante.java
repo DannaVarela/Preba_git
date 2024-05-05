6640658c95a1fd5ce5c26a5cbcc331318e8542ba
Danna <carovarela124@gmail.com>
2024-05-05 13:44:17 -0500

    Herencia - Implementación de Constructores por Danna Varela

--- Estudiante.java
+++ Estudiante.java
@@ -1,7 +1,8 @@
 public class Estudiante extends Persona{
     private String Grado;
-    public Estudiante(String Nombre, String FechaNacimiento, Integer Edad, String Grado){
+    public Estudiante(String Nombre,String FechaNacimiento, Integer Edad, String Grado) {
         super(Nombre,FechaNacimiento, Edad);
+        this.Grado = Grado;
     }
     public String toString() {
         return "Estudiante: " + this.getNombre() + ", Edad: " + this.getEdad() + ", Grado: " + this.Grado;
