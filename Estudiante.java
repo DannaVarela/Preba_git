c79688ff1a8a9585220b89a65eff734439e5cd9e
Danna <carovarela124@gmail.com>
2024-05-05 13:34:59 -0500

    Herencia - Sobrescritura del método toString() por Danna Varela

--- Estudiante.java
+++ Estudiante.java
@@ -3,4 +3,7 @@ public class Estudiante extends Persona{
     public Estudiante(String Nombre, String FechaNacimiento, Integer Edad, String Grado){
         super(Nombre,FechaNacimiento, Edad);
     }
+    public String toString() {
+        return "Estudiante: " + this.getNombre() + ", Edad: " + this.getEdad() + ", Grado: " + this.Grado;
+    }
 }
