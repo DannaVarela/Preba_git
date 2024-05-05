3cce54f3eb009e8594ab131c774ae483ffab9262
Danna <carovarela124@gmail.com>
2024-05-05 09:10:52 -0500

    Implementación de Métodos para Manipulación de Propiedades por Danna Varela

--- Persona.java
+++ Persona.java
@@ -2,4 +2,22 @@ public class Persona {
     private String Nombre;
     private String FechaNacimiento;
     private  Integer Edad;
+
+    public Persona(String Nombre, String FechaNacimiento, Integer Edad){
+        this.Nombre = Nombre;
+        this.FechaNacimiento = FechaNacimiento;
+        this.Edad = Edad;
+    }
+
+    public String getNombre() { return this.Nombre;}
+
+    public void setNombre(String nombre) { this.Nombre = nombre;}
+
+    public String getFechaNacimiento() { return this.FechaNacimiento;}
+
+    public void setFechaNacimiento(String fechaNacimiento) { this.FechaNacimiento = fechaNacimiento;}
+
+    public Integer getEdad() { return this.Edad;}
+
+    public void setEdad(Integer edad) { this.Edad = edad;}
 }
