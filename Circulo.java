183c3aa19d66e857d6cee4212264f1aa1bf6df09
Danna <carovarela124@gmail.com>
2024-05-07 07:40:53 -0500

    Encapsulamiento y Herencia - Implementación de Métodos para Cálculo de Área y Perímetro por Danna Varela

--- Circulo.java
+++ Circulo.java
@@ -1,3 +1,23 @@
 public class Circulo extends FiguraGeometrica{
     private float Radio;
+
+    public Circulo (String Nombre, String Color, float Radio){
+        super(Nombre,Color);
+        this.Radio = Radio;
+    }
+    public float getRadio() {
+        return this.Radio;
+    }
+
+    public void setRadio(float Radio) {
+        this.Radio = Radio;
+    }
+
+    public double calcularArea() {
+        return Math.PI * Math.pow(Radio, 2);
+    }
+    public double calcularPerimetro() {
+        return 2 * Math.PI * Radio;
+    }
+
 }
--- Rectangulo.java
+++ Rectangulo.java
@@ -1,4 +1,34 @@
 public class Rectangulo extends FiguraGeometrica{
     private float Longitud;
     private float Ancho;
+
+    public Rectangulo(String Nombre, String Color){
+        super(Nombre, Color);
+        this.Longitud = Longitud;
+        this.Ancho = Ancho;
+    }
+
+    public float getLongitud() {
+        return this.Longitud;
+    }
+
+    public void setLongitud(float longitud) {
+        this.Longitud = Longitud;
+    }
+
+    public float getAncho() {
+        return this.Ancho;
+    }
+
+    public void setAncho(float Ancho) {
+        this.Ancho = Ancho;
+    }
+    public float calcularArea(){
+        return this.Longitud * this.Ancho;
+    }
+    public float calcularPerimetro(){
+        return 2 * (this.Longitud + this.Ancho);
+
+    }
+
 }
