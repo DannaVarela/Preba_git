3c49cc363ec69ebec91aef1e230705455ae8324b
Danna <carovarela124@gmail.com>
2024-05-07 06:56:53 -0500

    Encapsulamiento y Herencia - Implementación de Métodos para Manipulación de Propiedades por Danna Varela

--- FiguraGeometrica.java
+++ FiguraGeometrica.java
@@ -1,4 +1,18 @@
 public class FiguraGeometrica {
     private String Nombre;
     private String Color;
+
+    public String getNombre() {
+        return this.Nombre;
+    }
+    public void setNombre(String Nombre){
+        this.Nombre = Nombre;
+    }
+    public  String getColor(){
+        return this.Color;
+    }
+    public  void setColor(){
+        this.Color = Color;
+    }
+
 }
